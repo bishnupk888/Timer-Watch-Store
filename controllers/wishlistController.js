309dc863e9cba96316e5d7582183bd188e95ed78
bishnupk888 <bishnupk888@gmail.com>
2023-11-11 04:27:04 +0530

    wishtest

--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -5,7 +5,7 @@ const getWishList = async (req, res) => {
     const wishlistCount = await wishListHelper.getWishListCount(user._id);
     wishListHelper.getWishListProducts(user._id).then((wishlistProducts) => {
 
-      res.render("wishList", {
+      res.render("wishlist", {
         user,
         wishlistProducts,
         wishlistCount,
